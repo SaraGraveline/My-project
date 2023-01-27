d312e33b310bb9e435a31fd380be6caf9cc9e37b
SaraGraveline <graveline.sara@gmail.com>
2023-01-27 12:11:55 -0500

    Unity Basic 2 - Completed
    In_class exercise  - Class 3
    
    Prefabs
    The game loop
    Input management
    Collision detection
    Physics, vector math

---
+++ Assets/rotator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour
+{
+    public float rotationSpeed = 0.3f;
+
+    private bool rotate;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame only use this for input
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            rotate = true;
+        }
+        else
+        {
+            rotate = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (rotate)
+        {
+           this.GetComponent<Transform>().Rotate(0, rotationSpeed, 0);
+        }
+        
+    }
+}
